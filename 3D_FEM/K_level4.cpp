8d73def8e68b903ec09c2a2bcfe2f055db9bfee9
13tm034 <k.kenji681@gmail.com>
2016-08-08 23:16:37 +0900

    debug with matlab

--- 3D_FEM/K_level4.cpp
+++ 3D_FEM/K_level4.cpp
@@ -61,6 +61,7 @@ void jacob(double J[][3], double dndr[][8], double nd[][8],double *r){		//201606
 	dndr[1][7] = (1 - r[0])*(1 + r[2]) * 0.125;
 	dndr[2][7] = (1 - r[0])*(1 + r[1]) * 0.125;
 
+	
 
 			for (int l = 0; l < 8; l++){
 				J[0][0] += dndr[0][l] * nd[0][l];
@@ -73,10 +74,12 @@ void jacob(double J[][3], double dndr[][8], double nd[][8],double *r){		//201606
 
 				J[2][0] += dndr[2][l] * nd[0][l];
 				J[2][1] += dndr[2][l] * nd[1][l];
-				J[2][2] += dndr[2][l] * nd[2][l];
-
+	
 			}
 
+
+
+	
 
 }
 
